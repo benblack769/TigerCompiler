93a032e85b8c62058ed54e27a665a05232724e1c
ben black <weepingwillowben@gmail.com>
2018-02-27 15:53:25 -0800

    added identifier tests

--- test_lexer.cc
+++ test_lexer.cc
@@ -11,7 +11,31 @@ TEST_CASE("Basic case", "[basic_case]") {
     REQUIRE(std::equal(expected.begin(), expected.end(), tok_list.begin()));
 }
 
-TEST_CASE("String testing", "[basic_case]") {
+TEST_CASE("Identifier testing", "[basic_case]") {
+    SECTION("check _main accepts") {
+        auto tok_list = build_token_list("_main");
+        REQUIRE(tok_list[0] == IDENTIFIER);
+        REQUIRE(tok_list.size() == 1);
+    }
+    SECTION("check starting underscores failure") {
+        auto tok_list = build_token_list("_argvar");
+        REQUIRE(tok_list[0] == BAD_TOKEN);
+    }
+    SECTION("check numbers in identifiers") {
+        auto tok_list = build_token_list("arg0var1");
+        REQUIRE(tok_list[0] == IDENTIFIER);
+        REQUIRE(tok_list.size() == 1);
+    }
+    SECTION("check normal identifiers") {
+        auto tok_list = build_token_list("Hi TheRe");
+        REQUIRE(tok_list[0] == IDENTIFIER);
+        REQUIRE(tok_list[1] == WHITESPACE);
+        REQUIRE(tok_list[2] == IDENTIFIER);
+        REQUIRE(tok_list.size() == 3);
+    }
+}
+
+TEST_CASE("String testing", "[string testing]") {
     SECTION("check no string termination") {
         auto tok_list = build_token_list("\"hi ");
         REQUIRE(tok_list[0] == BAD_TOKEN);
