a024a44b9f3afb65887c28b06d3805aa78fe9265
ben black <weepingwillowben@gmail.com>
2018-03-24 16:52:47 -0700

    fixed up gitignore

--- ast_interface.hh
+++ ast_interface.hh
@@ -35,7 +35,7 @@ protected:
   using ASTptr = const ASTNode*; // Can't use smart ptr in union :(
 
   ASTNode() = default;
-  virtual void set_source_loc(SourceLoc in_loc){ loc = in_loc;}
+  virtual void set_source_loc(SourceLoc in_loc){ loc = in_loc; }
   virtual ~ASTNode() = default;
   virtual void print(std::ostream & os) const = 0;
 };
