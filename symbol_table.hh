14e9d8f923d954256adb9a24fa29aec94ffa55f0
ben black <weepingwillowben@gmail.com>
2018-03-24 20:02:13 -0700

    improved reasabillity

--- symbol_table.hh
+++ symbol_table.hh
@@ -4,12 +4,14 @@ using namespace std;
 
 enum class BaseType { INT, VOID, STRING, ARRAY, RECORD };
 
+using typeid_ty = int;
+
 struct TypeExpr{
     BaseType type;
     //arrays store name of type
-    int array_type;
+    typeid_ty array_type;
     //maps from name feild to type field (entry in type table)
-    unordered_map<string,int> record_type;
+    unordered_map<string,typeid_ty> record_type;
     TypeExpr(BaseType b_type){
         assert(b_type == BaseType::INT ||
                b_type == BaseType::STRING ||
@@ -32,19 +34,22 @@ struct TypeExpr{
     }
 };
 struct TypeTable{
-    int typenum = 0;
-    unordered_map<int, TypeExpr> typevals;
-    unordered_map<string, int> types;
+    typeid_ty typenum = 0;
+    unordered_map<typeid_ty, TypeExpr> typevals;
+    unordered_map<string, typeid_ty> types;
     TypeTable(){
         add_type("string", TypeExpr(BaseType::STRING));
         add_type("int", TypeExpr(BaseType::INT));
     }
-    void add_type(string tyid, const TypeExpr & ty_expr){
-        types[tyid] = typenum;
-        typevals[typenum] = ty_expr;
+    typeid_ty add_type(string tyid, const TypeExpr & ty_expr){
+        typeid_ty cur_type = typenum;
         typenum += 1;
+
+        types[tyid] = cur_type;
+        typevals[typenum] = ty_expr;
+        return cur_type;
     }
-    const int lookup(string tyid){
+    const typeid_ty lookup(string tyid){
         return types[tyid];
     }
 };
