5983c16cb2f71b61739b1454587483b8edf25e50
Alex <AlexV348@gmail.com>
2018-04-08 19:53:15 +0000

    remove string translate

--- ast_translate.cc
+++ ast_translate.cc
@@ -7,61 +7,7 @@
 
 namespace tiger{
 
-// will probably get this from another file in the future
-const int wordSize = 4;
-
-// Build up a tree of ESEQ and MOVE nodes to
-// move all of the characters to the correct places
-IRTptr exprs::StringNode::translate() const{
-    // this will hold the location of the start of the string
-    // probably will be set to the stack pointer in the future
-    ir::const_t strStart = 0;
-    // this is the variable that will be used to count up each word in the string
-    ir::const_t strP = strStart;
-
-    // this will store each of our memory calls before we make it into a tree
-    std::vector<shared_ptr<ir::Move>> strExps = {};
-    
-    // the first word in the string will be it's length
-    ir::const_t length = mystring.length();
-    auto lNode = std::make_shared<ir::Move>(std::make_shared<ir::Mem>(std::make_shared<ir::Const>(strP)), std::make_shared<ir::Const>(length));
-    strExps.push_back(lNode);
-    strP++;
-    
-    // we can fit four characters in a word, so currWord will count how we're doing
-    // we use shifting to fit them all 
-    ir::const_t currWord = 0;
-    int charInWord = 0;
-    for (int i = 0; i < length; i++) {
-        currWord += static_cast<ir::const_t>(mystring[i]);
-        charInWord++;
-        // once a word is filled with chars, we start a new word
-        if (charInWord >= wordSize){
-            auto cNode = std::make_shared<ir::Move>(std::make_shared<ir::Mem>(std::make_shared<ir::Const>(strP)), std::make_shared<ir::Const>(currWord));
-            strExps.push_back(cNode);
-            strP++;
-            currWord = 0; 
-            charInWord = 0;
-        }
-        currWord = currWord << 8; //shift word one byte
-    }
-    // finish up our last word
-    if (currWord != 0) {
-        auto cNode = std::make_shared<ir::Move>(std::make_shared<ir::Mem>(std::make_shared<ir::Const>(strP)), std::make_shared<ir::Const>(currWord));
-        strExps.push_back(cNode);
-    }
-    // build up a tree of ESEQs where the whole tree will return the location of the beginning
-    // of the string. 
-    // in other words, the terminal right leaf of the tree will contain the location
-    std::shared_ptr<ir::IRTNode> rNode = std::make_shared<ir::Const>(strStart);
-    for (int i = strExps.size()-1; i >= 0; i--){
-        IRTptr oldRNode = rNode;
-        rNode = std::make_shared<ir::Eseq>(strExps[i], oldRNode);
-    }
-    
-    return rNode;
-}
-
+IRTptr exprs::StringNode::translate() const{return nullptr;}
 IRTptr exprs::NilNode::translate() const{return nullptr;}
 IRTptr exprs::IntNode::translate() const{return nullptr;}
 IRTptr exprs::LvalNode::translate() const{return nullptr;}
