898887f7c42f62dab03209efea8a55602b489651
ben black <weepingwillowben@gmail.com>
2018-04-09 21:17:59 -0700

    added conditional code

--- ast_translate.cc
+++ ast_translate.cc
@@ -7,19 +7,101 @@
 
 FrameStack full_frame;
 
-namespace tiger{
+using namespace tiger;
+using namespace ir;
 
 IRTptr exprs::StringNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::NilNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::IntNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::LvalNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::NegateNode::translate(SymbolTable & env) const{return nullptr;}
-IRTptr exprs::BinaryNode::translate(SymbolTable & env) const{return nullptr;}
+
+expPtr to_expr_seq(stmPtrList list,expPtr expr){
+    expPtr res_expr = expr;
+    for(stmPtr st : list){
+        res_expr = to_expPtr(Eseq(st,res_expr));
+    }
+    return res_expr;
+}
+ir::expPtr compile_conditional(rel_op_k oper, ir::expPtr left, ir::expPtr right, ir::expPtr then_expr, ir::expPtr else_expr){
+    Temp_temp result = newtemp();
+
+    Temp_label then_l = newlabel();
+    Temp_label else_l = newlabel();
+    Temp_label end_l = newlabel();
+
+    stmPtrList stmts = {
+        to_stmPtr(CJump(oper,left,right,then_l,else_l)),
+        to_stmPtr(Label(then_l)),
+        to_stmPtr(Move(to_expPtr(Temp(result)), then_expr)),
+        to_stmPtr(Jump(end_l)),
+        to_stmPtr(Label(else_l)),
+        to_stmPtr(Move(to_expPtr(Temp(result)), else_expr)),
+        to_stmPtr(Label(end_l)),
+    };
+    return to_expr_seq(stmts, to_expPtr(Temp(result)));
+}
+rel_op_k to_rel_op(exprs::BinaryOp op){
+    using namespace  exprs;
+    switch(op){
+    case BinaryOp::EQUAL: return rel_op_k::EQ;
+    case BinaryOp::LESS: return rel_op_k::LT;
+    case BinaryOp::LESSEQ: return rel_op_k::LE;
+    case BinaryOp::GREATER: return rel_op_k::GT;
+    case BinaryOp::GREATEREQ: return rel_op_k::GE;
+    case BinaryOp::LESSGREATER: return rel_op_k::NE;
+    default:
+        assert(false && "bad binop to to_rel_op");
+    }
+    return rel_op_k::EQ;
+}
+IRTptr exprs::BinaryNode::translate(SymbolTable & env) const{
+    if(op == BinaryOp::ADD
+            || op == BinaryOp::SUB
+            || op == BinaryOp::MUL
+            || op == BinaryOp::DIV){
+
+    }
+    else if(op == BinaryOp::AND ||
+            op == BinaryOp::OR){
+        expPtr left_e = cast_to_exprPtr(left->translate(env));
+        expPtr right_e = cast_to_exprPtr(right->translate(env));
+        expPtr one_e = to_expPtr(Const(1));
+        expPtr zero_e = to_expPtr(Const(0));
+        if(op == BinaryOp::OR){
+            return compile_conditional(rel_op_k::EQ,
+                                       left_e,
+                                       one_e,
+                                       one_e,
+                                       right_e);
+        }
+        else{
+            return compile_conditional(rel_op_k::EQ,
+                                       left_e,
+                                       zero_e,
+                                       zero_e,
+                                       right_e);
+        }
+    }
+    else{
+        return compile_conditional(to_rel_op(op),
+                                   cast_to_exprPtr(left->translate(env)),
+                                   cast_to_exprPtr(right->translate(env)),
+                                   to_expPtr(Const(1)),
+                                   to_expPtr(Const(0)));
+    }
+}
 IRTptr exprs::AssignNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::FunctionCall::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::ExprSequenceEval::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::IfThen::translate(SymbolTable & env) const{return nullptr;}
-IRTptr exprs::IfThenElse::translate(SymbolTable & env) const{return nullptr;}
+IRTptr exprs::IfThenElse::translate(SymbolTable & env) const{
+    return compile_conditional(rel_op_k::EQ,
+                               to_expPtr(Const(0)),
+                               cast_to_exprPtr(_cond->translate(env)),
+                               cast_to_exprPtr(_res_1->translate(env)),
+                               cast_to_exprPtr(_res_2->translate(env)));
+}
 IRTptr exprs::WhileDo::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::ForToDo::translate(SymbolTable & env) const{return nullptr;}
 IRTptr exprs::Break::translate(SymbolTable & env) const{return nullptr;}
@@ -34,11 +116,11 @@ ir::expPtr translate_variable(F_access acc, int level){
     int cur_level = full_frame.current_level();
 
     assert(level <= cur_level);
-    ir::expPtr cur_link_expr = make_shared<ir::Temp>(F_FP());
+    ir::expPtr cur_link_expr = to_expPtr(ir::Temp(F_FP()));
     for(int l = cur_level; l > level; l--){
         F_access static_link_acc = get_static_link(l);
-        ir::expPtr new_link_expr = F_Exp(static_link_acc, make_shared<ir::Temp>(F_FP()));
-        cur_link_expr.swap(new_link_expr);
+        ir::expPtr new_link_expr = F_Exp(static_link_acc, cur_link_expr);
+        cur_link_expr = new_link_expr;
     }
     return F_Exp(acc,cur_link_expr);
 }
@@ -52,11 +134,11 @@ IRTptr lvals::BracketAccess::translate(SymbolTable & env) const{return nullptr;}
 IRTptr decls::VarDecl::translate(SymbolTable & env) const{return nullptr;}
 IRTptr decls::FuncDecl::translate(SymbolTable & env) const{return nullptr;}
 IRTptr decls::TypeDecl::translate(SymbolTable & env) const{return nullptr;}
-/*
+
 IRTptr types::BasicType::translate(SymbolTable & env) const{return nullptr;}
 IRTptr types::ArrayType::translate(SymbolTable & env) const{return nullptr;}
 IRTptr types::TypeFeildType::translate(SymbolTable & env) const{return nullptr;}
-*/
+
 IRTptr ExprListNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr ExprSequenceNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr FieldNode::translate(SymbolTable & env) const{return nullptr;}
@@ -67,24 +149,8 @@ IRTptr DeclarationListNode::translate(SymbolTable & env) const{return nullptr;}
 IRTptr TypeFeildsNode::translate(SymbolTable & env) const{return nullptr;}
 
 //conditional evaluation helper function (used for boolean operations like | as well as if statements)
-/*T_expr compile_conditional(RelOp, left, right, then_expr, else_expr){
-	result = Temp_temp()
 
-	then = new_label()
-	else = new_label()
-	end = new_label()
-
-	stmts = {
-		T_Cjump(op,left,right,then,else),
-		T_Label(then),
-		T_Move(T_Temp(result), then_expr),
-		T_Label(else),
-		T_Move(T_Temp(result), else_expr),
-		T_Label(end),
-	}
-	T_Eseq(stmts, T_Temp(result))
-}*/
-//variable_access
+//function access
 /*T_expr get_function_static_link(int func_level){
     int cur_level = get_current_level();
 
@@ -99,5 +165,3 @@ IRTptr TypeFeildsNode::translate(SymbolTable & env) const{return nullptr;}
         //
     }
 }*/
-
-} //namespace
