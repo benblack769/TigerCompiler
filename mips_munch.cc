c222fa5383208c37d1f9a8b8d9c709322daabbba
ben black <weepingwillowben@gmail.com>
2018-04-19 20:11:08 -0700

    edits and made compile

--- mips_munch.cc
+++ mips_munch.cc
@@ -9,7 +9,7 @@ const string gp_register_1 = "$t0";
 const string gp_register_2 = "$t1";
 const string stack_pointer = "$sp";
 const int word_size = 4;
-const int word_size_str = to_string(word_size);
+const string word_size_str = to_string(word_size);
 
 string format_instruction(string instr, string arg1="", string arg2="", string arg3=""){
     string res = instr + "\t\t";
@@ -28,17 +28,6 @@ string format_instruction(string instr, string arg1="", string arg2="", string a
 string indirect_acc(string ptr_reg, int offset=0){
     return to_string(offset) + "(" + ptr_reg + ")";
 }
-
-std::string Const::munch(){return "";}
-std::string Name::munch(){return "";}
-std::string Temp::munch(){return "";}
-std::string BinOp::munch(){return "";}
-std::string Mem::munch(){return "";}
-std::string Call::munch(){return "";}
-std::string Eseq::munch(){return "";}
-std::string Move::munch(){return "";}
-std::string Exp::munch(){return "";}
-std::string Jump::munch(){return "";}
 string pop_into(string reg){
     string line_1 = format_instruction("sw", reg, indirect_acc(stack_pointer));
     string line_2 = format_instruction("addiu",stack_pointer,stack_pointer,"-"+word_size_str);
@@ -49,14 +38,33 @@ string push_onto(string reg){
     string line_2 = format_instruction("sw", reg, indirect_acc(stack_pointer));
     return line_1 + line_2;
 }
+
+std::string Const::munch(){return "";}
+std::string Name::munch(){return "";}
+std::string Temp::munch(){return "";}
+std::string BinOp::munch(){return "";}
+std::string Mem::munch(){return "";}
+std::string Call::munch(){return "";}
+std::string Eseq::munch(){return "";}
+std::string Move::munch(){return "";}
+std::string Exp::munch(){
+    return exp_->munch() +
+           pop_into(gp_register_1);
+}
+std::string Jump::munch(){
+    return lab_.toString();
+}
 string instr_name(rel_op_k op){
     switch(op){
-        case rel_op_k::EQ: "beq";
-        case rel_op_k::LT: "blt";
-        case rel_op_k::LE: "ble";
-        case rel_op_k::GT: "bgt";
-        case rel_op_k::GE: "bge";
-        case rel_op_k::NE: "bne";
+    case rel_op_k::EQ: return "beq";
+    case rel_op_k::LT: return "blt";
+    case rel_op_k::LE: return "ble";
+    case rel_op_k::GT: return "bgt";
+    case rel_op_k::GE: return "bge";
+    case rel_op_k::NE: return "bne";
+    default:
+        assert(false && "bad case");
+        return "";
     }
 }
 std::string CJump::munch(){
@@ -64,13 +72,13 @@ std::string CJump::munch(){
         right_->munch() +
         pop_into(gp_register_2) +
         pop_into(gp_register_1) +
-        format_instruction(instr_name(this->op_),gp_register_1,gp_register_2,trueLab_) +
-        format_instruction("b",falseLab_);
+        format_instruction(instr_name(this->op_),gp_register_1,gp_register_2,trueLab_.toString()) +
+        format_instruction("b",falseLab_.toString());
 }
 std::string Seq::munch(){
-    return stm1_.munch() +
-           stm2.munch();
+    return stm1_->munch() +
+           stm2_->munch();
 }
 std::string Label::munch(){
-    return name_+":\n";
+    return name_.toString()+":\n";
 }
